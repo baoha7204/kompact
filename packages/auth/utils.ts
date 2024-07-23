7af58ead5ab62a6acba582316d1e59e6d260def1
transang <tranquangsang12.7@gmail.com>
2024-07-23 10:18:30 +0700

    Feature: Add utils auth generate token

--- packages/auth/utils.ts
+++ packages/auth/utils.ts
@@ -1,9 +1,45 @@
 import crypto from "crypto";
+import jwt from "jsonwebtoken";
 
-class Auth {
+export type KeyPair = {
+  publicKey: string;
+  privateKey: string;
+};
+export type CreateTokenPairValidOptions = {
+  accessTokenExpiresIn: string | number;
+  refreshTokenExpiresIn: string | number;
+};
+
+class AuthUtils {
   static generateKeyPair() {
-    // crypto.generateKeyPairSync("rsa");
+    const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
+      modulusLength: 4096,
+    });
+    return {
+      privateKey,
+      publicKey,
+    };
+  }
+  static createTokenPair(
+    payload: Record<string, any>,
+    privateKey: any,
+    { accessTokenExpiresIn, refreshTokenExpiresIn }: CreateTokenPairValidOptions
+  ) {
+    // create accessToken using private key
+    const accessToken = jwt.sign(payload, privateKey, {
+      algorithm: "RS256",
+      expiresIn: accessTokenExpiresIn,
+    });
+    // create refreshToken using private key
+    const refreshToken = jwt.sign(payload, privateKey, {
+      algorithm: "RS256",
+      expiresIn: refreshTokenExpiresIn,
+    });
+    return {
+      accessToken,
+      refreshToken,
+    };
   }
 }
 
-export default Auth;
+export default AuthUtils;
